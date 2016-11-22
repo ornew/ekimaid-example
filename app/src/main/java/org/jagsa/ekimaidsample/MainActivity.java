df002277bdded9579fc3e44ec9cf693253a43d39
Arata Furukawa <info@ornew.net>
2016-11-23 00:37:25 +0900

    Format comment

--- app/src/main/java/org/jagsa/ekimaidsample/MainActivity.java
+++ app/src/main/java/org/jagsa/ekimaidsample/MainActivity.java
@@ -12,13 +12,18 @@ import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText edit_search_word;
-    private TextView text_output;
-    private Button button_search;
-    private SearchStationTask task;
+/**
+ * Created on 11/22, 2016.
+ * Copyright 2016 Arata Furukawa <old.river.new@gmail.com>.
+ */
 
-    // 出力を表示するためのテキストビューに文字列を追記する関数です
+public class MainActivity extends AppCompatActivity {
+    private EditText          edit_search_word; // 検索する文字を入力するエディットテキストビューです
+    private TextView          text_output;      // プログラムからの出力を表示するためのテキストビューです
+    private Button            button_search;    // 検索ボタンのビューです
+    private SearchStationTask task;             // 検索をする非同期タスクです
+
+    // 出力を表示するためのテキストビュー(text_output)に文字列を追記する関数です
     private void print(String text) {
         text_output.setText(text_output.getText() + text);
     }
@@ -28,40 +33,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 検索する文字を入力するエディットビューを取得します
+        // 画面上のビューを取得してメンバ変数に保存します
         edit_search_word = (EditText) findViewById(R.id.edit_search_word);
-        // 出力を表示するためのテキストビューを取得します
-        text_output = (TextView) findViewById(R.id.text_output);
-        // 検索ボタンを取得します
-        button_search = (Button) findViewById(R.id.button_search);
+        text_output      = (TextView) findViewById(R.id.text_output);
+        button_search    = (Button)   findViewById(R.id.button_search);
 
-        // 検索ボタンがタップされたときの動作を記述します
+        // 検索ボタンがタップされたときの動作を登録します
         button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 検索ボタンをオフにします
-                // 連打されて何度も通信が発生してしまうことを防ぎます
+                // 検索ボタンを無効にして、連打されて何度も通信が発生してしまうことを防ぎます
                 button_search.setEnabled(false);
-                // 出力をクリアします
+                // 出力を空にします
                 text_output.setText("");
+
                 // 非同期検索タスクを作成します
                 task = new SearchStationTask() {
                     // onPostExecuteメソッドをオーバーライドします
                     // このメソッドはタスクの実行が終わった後に実行されます
+                    // 引数は通信で取得したJSONオブジェクトです(nullの場合はエラーです)
                     @Override
                     protected void onPostExecute(JSONObject results) {
                         super.onPostExecute(results);
+
+                        // エラーの場合はnullになるようにしてあるため、nullでないかチェックをします
                         if (results != null) {
-                            // resultsがnullでないときは結果が入っているので
-                            // 中身を取得します
+                            // null出ない場合は結果が入っていますので、ここで中身を取得・操作します
+                            // JSONオブジェクトの操作に失敗するとJSONExceptionという例外が発生するので
+                            // try-catch構文で例外処理をする必要があります
                             try {
+                                //==========================================
                                 // 解析はせずにJSONをそのまま表示します
                                 print(results.toString(2));
+                                //==========================================
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                         }
-                        // 通信の成功・失敗にかかわらず、ボタンを再びオンにします
+
+                        // 通信の成功・失敗にかかわらず、ボタンを再び有効にします
                         button_search.setEnabled(true);
                     }
 
@@ -71,16 +81,17 @@ public class MainActivity extends AppCompatActivity {
                         print("エラーが発生しました。");
                     }
                 };
-                // 入力エディットに入力されている文字を取得します
+
+                // エディットに入力されている文字を取得します
                 String serach_word = edit_search_word.getText().toString();
-                if(serach_word != null && !serach_word.isEmpty()) {
+                if(!serach_word.isEmpty()) {
                     // 入力が空でなければ非同期検索タスクを実行します
                     // 引数は検索する文字列にしてあります(詳しくは、SearchTaskを見てください)
                     task.execute(serach_word);
                 } else {
                     // 入力が空だった場合は検索を実行せずにメッセージを出力します
                     print("検索する文字を入力してください。");
-                    // ボタンを再びオンにします
+                    // ボタンを再び有効にします
                     button_search.setEnabled(true);
                 }
             }
@@ -94,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
                 (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo info = connectivity.getActiveNetworkInfo();
         if (info == null || !info.isConnected()) {
-            // ネットワークに接続で来ていない場合はボタンをオフにして、メッセージをだす
+            // ネットワークに接続できていない場合はボタンを無効にしてメッセージを表示します
             button_search.setEnabled(false);
             print("ネットワークに接続していません。ネットワークに接続して、アプリを再起動してください。");
         }

11d7fd6fb66fa05cd4e99ff93670e8536780864f
Arata Furukawa <info@ornew.net>
2016-12-04 04:56:11 +0900

    Breaking Changes: To simple, Add redundancy comments.

--- app/src/main/java/org/jagsa/ekimaidsample/HTTP.java
+++ app/src/main/java/org/jagsa/ekimaidsample/HTTP.java
@@ -13,27 +13,53 @@ import java.net.URL;
  */
 
 class HTTP {
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // URLにHTTP通信でリクエストを送るメソッドを定義します
+    //
+    //   引　数: URL    -> 通信したいURLです
+    //   戻り値: String -> 通信で取得したレスポンスの文字列です
+    //
+    // --- 注意 ---------------------------------------------------------------------------
+    //
+    //  HTTP通信のように、Androidアプリでインターネット接続を行う場合は、AndroidManifest.xmlの
+    //  manifestタグに以下のようにパーミッションを宣言する必要があります。
+    //
+    //    <uses-permission android:name="android.permission.INTERNET" />
+    //
+    //  詳しくは、AndroidManifest.xmlを見てください。
+    // --------------------------------------------------------------------------------------
+    //
+    // 難しい話
+    //   - HTTPメソッドはGETに固定しています(今回はGET以外扱いません)
+    //   - エラーが発生した場合、キャッチせずにそのまま呼び出し元に投げます。
+    //     例外ハンドリングは呼び出し元で行います。
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     static String request(URL url) throws IOException {
+        HttpURLConnection connection = null;
         BufferedReader    reader     = null;
         InputStream       in;
-        HttpURLConnection connection = null;
         try {
-            // URLに対するHTTP通信を開きます
-            connection = (HttpURLConnection) url.openConnection();
-            // HTTPリクエストはGETを指定します
-            connection.setRequestMethod("GET");
-            // 通信を接続します
-            connection.connect();
+            connection = (HttpURLConnection) url.openConnection(); // URLに対するHTTP通信を開きます
+            connection.setRequestMethod("GET");                    // HTTPリクエストはGETで固定します
+            connection.connect();                                  // 通信を接続します
 
-            // レスポンスを取得するためのストリームを開きます
+
             try {
+                // レスポンスを取得するためのストリームを開きます
                 in = connection.getInputStream();
             } catch (IOException e){
+                // ストリームが開けない場合、エラーストリームを開きます
                 in = connection.getErrorStream();
             }
+            // ストリームがどちらも開けなかった場合は例外を投げます
             if(in == null){
                 throw new IOException("The stream could not be opened.");
             }
+
+            // ストリームからレスポンスを読み取るために、便利なバッファリーダーを使います
+            // 面倒な処理を代わりにやってくれるおまじないだと思ってください
             reader = new BufferedReader(new InputStreamReader(in));
 
             // レスポンスを文字列に変換します
@@ -42,6 +68,8 @@ class HTTP {
             while ((line = reader.readLine()) != null) {
                 response.append(line).append("\n");
             }
+
+            // レスポンスを戻り値として返します
             return response.toString();
         } finally {
             // 接続を閉じます
--- app/src/main/java/org/jagsa/ekimaidsample/MainActivity.java
+++ app/src/main/java/org/jagsa/ekimaidsample/MainActivity.java
@@ -1,7 +1,6 @@
 package org.jagsa.ekimaidsample;
 
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,64 +11,30 @@ import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Created on 11/22, 2016.
  * Copyright 2016 Arata Furukawa <old.river.new@gmail.com>.
  */
 
 public class MainActivity extends AppCompatActivity {
-    private EditText            edit_search_word;    // 検索する文字を入力するエディットテキストビューです
-    private TextView            text_output;         // 出力を表示するためのテキストビューです
-    private Button              button_search;       // 検索ボタンのビューです
-    private MySearchStationTask task_search_station; // 検索をする非同期タスクです
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // メンバ変数を宣言します
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    private EditText               edit_search_word;    // 検索する文字を入力するためのエディットビューです
+    private TextView               text_output;         // 出力を表示するためのテキストビューです
+    private Button                 button_search;       // 検索ボタンのビューです
+    private AsyncSearchStationTask task_search_station; // 駅を検索をする非同期タスクです
 
-    // 出力を表示するためのテキストビュー(text_output)に文字列を追記する関数です
-    private void print(String text) {
-        text_output.setText(text_output.getText() + text);
-    }
-    private void println(String text) {
-        print(text + '\n');
-    }
-
-    // タスクの実行中にエラーが発生した場合に行う表示とボタンの復帰処理です
-    private void errorOnTask(){
-        println("エラーが発生しました。");
-        println("詳細はスタックトレースをAndroid StudioのLogcatで確認してください。");
-        button_search.setEnabled(true);
-    }
-
-    // 駅を検索する非同期タスクを継承してこのアクティビティ用にカスタマイズします
-    private class MySearchStationTask extends SearchStationTask {
-        // onPostExecuteメソッドをオーバーライドします
-        // このメソッドはタスクの実行が終わった後に実行されます
-        // 引数は通信で取得したJSONオブジェクトです(nullの場合はエラーです)
-        @Override
-        protected void onPostExecute(JSONObject results) {
-            super.onPostExecute(results);
-
-            // 通信エラーの場合はnullになるようにしてあるため、nullでないかチェックをします
-            if (results != null) {
-                // nullでない場合は結果が入っていますので、ここで中身を取得・操作します
-                // JSONオブジェクトの操作に失敗するとJSONExceptionという例外が発生するので
-                // try-catch構文で例外処理をする必要があります
-                try {
-                    //==========================================
-                    // 解析はせずにJSONをそのまま表示します
-                    println(results.toString(2));
-                    //==========================================
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    errorOnTask();
-                }
-            } else {
-                // nullの場合はエラーが発生しています
-                errorOnTask();
-            }
-            // 成功・失敗にかかわらず、ボタンを有効に戻します
-            button_search.setEnabled(true);
-        }
-    }
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // アプリが実行されたときに行う処理を定義します
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,12 +50,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 button_search.setEnabled(false);  // 検索ボタンを無効にして連打されないようにします
-                text_output.setText("");          // 出力を空にします
+                text_output.setText("");          // 画面に表示される文字を空にします
 
-                // 駅を検索する非同期タスクを作成します
-                task_search_station = new MySearchStationTask();
-
-                // エディットに入力されている文字を取得します
+                // 入力欄(テキストエディット)に入力されている文字を取得します
                 String serach_word = edit_search_word.getText().toString();
 
                 if(serach_word.isEmpty()) {
@@ -98,11 +60,150 @@ public class MainActivity extends AppCompatActivity {
                     print("検索する文字を入力してください。");
                     button_search.setEnabled(true); // ボタンを有効に戻します
                 } else {
-                    // 入力が空でなければ非同期検索タスクを実行します
-                    // 引数は検索する文字列にしてあります(詳しくは、SearchStationTaskを見てください)
+                    // 入力が空でなければ非同期に駅を検索するタスクを作成します
+                    task_search_station = new AsyncSearchStationTask();
+
+                    // タスクを実行します
+                    // 引数は検索する文字列にしてあります(詳しくは、AsyncSearchStationTaskを見てください)
                     task_search_station.execute(serach_word);
                 }
             }
         });
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // 駅を検索する非同期タスクのクラス(AsyncSearchStationTask)を定義します
+    //
+    // 非同期タスクを作るために、AsyncTaskを継承します。
+    // AsyncTaskのあとの<から>までには、カンマ区切りで順番に
+    //   1. タスクの引数の型
+    //   2. タスクのonProgressUpdateに渡されるの型(今回は使いません)
+    //   3. タスクのonPostExecuteに渡される型
+    // を指定します。
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    private class AsyncSearchStationTask extends AsyncTask<String, Void, String> {
+        private String error_message = ""; // 通信中にエラーが起きた場合に保存するメッセージ文字列です
+
+        //------------------------------------------------------------------------------------------
+        // doInBackgroundメソッドをオーバーライドします
+        //
+        //   引　数: String... -> 検索したい文字列
+        //   戻り値: String    -> 通信で取得したレスポンスの文字列
+        //
+        // このメソッドは非同期に実行されます。
+        // このメソッドの中でUIに関わる操作はできません。
+        // 引　数の型は、AsyncTaskに指定した１つ目の型と同じにする必要があります。
+        // 戻り値の型は、AsyncTaskに指定した３つ目の型と同じにする必要があります。
+        //------------------------------------------------------------------------------------------
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                // APIのリクエストのためのURLを作ります
+                // ここでは駅すぱあとの簡易駅情報取得APIを呼び出します
+                // APIの仕様は公式リファレンスを読んで下さい
+                // 公式リファレンス： http://docs.ekispert.com/v1/le/station/light.html
+                // ここでは、クエリパラメータnameに引数で受け取った検索文字を指定します
+                // 駅すぱあとのAPIリクエストには、APIキーをクエリパラメータkeyに指定する必要があります
+                URL api_request_url = new URL(
+                        "http://api.ekispert.jp/v1/json/station/light"
+                                + "?key=" + APIKEY.ekispert
+                                + "&name=" + strings[0]);
+
+                // URLにHTTP通信でリクエストを送信します
+                // HTTP.requestメソッドはHTTP.javaに定義してあります
+                String response = HTTP.request(api_request_url);
+
+                // レスポンスの文字列を戻り値として返します
+                return response;
+
+            } catch (IOException e) {
+                // 通信中にエラーが発生しました
+                e.printStackTrace();
+                error_message = e.toString();
+
+                // 今回は、通信エラーが発生した場合はnullを返すようにします
+                return null;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------
+        // onPostExecuteメソッドをオーバーライドします
+        //
+        //   引　数: String -> 通信で取得したレスポンスの文字列
+        //                    (通信中にエラーが発生した場合はnullが渡されます)
+        //   戻り値: なし
+        //
+        // このメソッドはタスクの実行が終わった後に実行されます。
+        // このメソッドの中ではUIに関わる操作を行うことができます。
+        //------------------------------------------------------------------------------------------
+        @Override
+        protected void onPostExecute(String results) {
+            super.onPostExecute(results);
+
+            // 通信エラーの場合はnullになるようにしてあるため、nullでないかチェックをします
+            if (results != null) {
+                // nullでない場合は結果が入っていますので、ここで中身を取得・操作します
+                // JSONオブジェクトの操作に失敗するとJSONExceptionという例外が発生するので
+                // try-catch構文で例外処理をする必要があります
+                try {
+                    //##############################################################################
+                    //##############################################################################
+
+                    // レスポンスの文字をJSONとして解析します
+                    JSONObject json = new JSONObject(results);
+
+                    // それ以上の解析はせずにJSONをそのまま表示します
+                    println(json.toString(2));
+
+                    //##############################################################################
+                    //##############################################################################
+                } catch (JSONException e) {
+                    // JSONの解析中にエラーが発生しました
+                    e.printStackTrace();
+                    errorOnTask(e.toString());
+                }
+            } else {
+                // resultsがnullの場合は、通信中にエラーが発生しましたので、メッセージを表示します
+                // エラーを示す文字はerror_messageに入れるようになっています。
+                errorOnTask(error_message);
+            }
+            // 成功・失敗にかかわらず、ボタンを有効に戻します
+            // 失敗した場合もちゃんとボタンを有効に戻さないと、二度と検索できなくなりますね
+            button_search.setEnabled(true);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // アプリの画面に文字を表示するメソッドを定義します
+    //
+    //   引　数: String -> 表示したい文字列
+    //   戻り値: なし
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    private void print(String text) {
+        text_output.setText(text_output.getText() + text);
+    }
+    private void println(String text) {
+        print(text + '\n');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // エラーが発生した時に表示するためのメソッドを定義します
+    //
+    //   引　数: String -> エラーメッセージです
+    //   戻り値: なし
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    private void errorOnTask(String message){
+        println("エラーが発生しました: ");
+        println(message);
+        println("より詳しい情報については、Android StudioのLogcatからスタックトレースを確認してください。");
+
+        // 念のため、ボタンを有効に戻します
+        button_search.setEnabled(true);
     }
 }
